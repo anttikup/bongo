562d0d97cdcc81738abcb8c5bc35a4a278d6bf42
AK <anttikup@metropolia.fi>
2022-12-02 17:44:01 +0200

    Added more air after heading

--- src/pages/exercise/[topic]/[level].tsx
+++ src/pages/exercise/[topic]/[level].tsx
@@ -30,6 +30,7 @@ const ExercisePage = (props: Props) => {
     const router = useRouter();
     const topic = router.query.topic as string;
     const level = router.query.level as string;
+    let topicTitle = topic;
 
     const [health, setHealth] = useState(3);
     const [questionSet, setQuestionSet] = useState<QuestionSet>([]);
@@ -59,6 +60,7 @@ const ExercisePage = (props: Props) => {
                 const questionSetFromApi = await exerciseService.getQuestionSet({ topic, level: parseInt(level, 10) });
                 console.assert(questionSetFromApi.length >= health, `Must have at least ${health} questions`);
                 setQuestionSet(questionSetFromApi);
+                topicTitle = questionSetFromApi[0].title;
             } catch (e) {
                 console.error(e);
                 setQuestionSet([]);
@@ -161,11 +163,11 @@ const ExercisePage = (props: Props) => {
                 }
             </Head>
             <Header as="header">
-                <h2 className={styles.title}>
+                <h1 className={styles.title}>
                     <span className={styles.supertitle}>Exercise</span>
                     <br/>
-                    <span className={styles.exerciseTitle}>{topic} {level}</span>
-                </h2>
+                    <span className={styles.exerciseTitle}>{topicTitle} {level}</span>
+                </h1>
                 <Health max={3} value={health} />
             </Header>
 
