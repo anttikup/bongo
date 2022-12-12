bb95cf3bf32e479d94d2ee2d285ee6f5823409f9
AK <anttikup@metropolia.fi>
2022-12-12 14:14:56 +0200

    Removed extra div

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -19,8 +19,7 @@ type NavMenuProps = {
 function NavMenu(props: NavMenuProps) {
     const [activeItem, setActiveItem] = useState('Laptop Item')
     const [showModal, setShowModal] = useState(false)
-    const isMobile = useMediaQuery({ query: '(max-width: 991px)' })
-    const isDesktop = useMediaQuery({ query: '(min-width: 992px)' })
+    const isMobile = useMediaQuery({ query: '(max-width: 991px)' });
     const [{ experience, user }, dispatch] = useStateValue();
 
     const handleSignin = (e: MouseEvent<HTMLAnchorElement>) => {
@@ -35,116 +34,113 @@ function NavMenu(props: NavMenuProps) {
 
 
     return (
-        <div>
-            <Menu as="nav">
+        <Menu as="nav">
 
-                { isDesktop &&
-                  <>
-                      <Menu.Item>
+            { !isMobile &&
+              <Menu.Menu key="desktop">
+                  <Menu.Item>
+                      <Link href="/">
+                          Home
+                      </Link>
+                  </Menu.Item>
+                  { user
+                    && <Menu.Item>
+                        <Link href="/overview">
+                            Overview
+                        </Link>
+                    </Menu.Item>
+                  }
+                  { user
+                    && <Menu.Item>
+                        <Link href="/lectures">
+                            Lectures
+                        </Link>
+                    </Menu.Item>
+                  }
+                  { user
+                    && <Menu.Item>
+                        <Link href="/stats">
+                            Stats
+                        </Link>
+                    </Menu.Item>
+                  }
+                  { user
+                    && <Menu.Item>
+                        <Link href="/settings">
+                            Settings
+                        </Link>
+                    </Menu.Item>
+                  }
+              </Menu.Menu>
+            }
+            { isMobile &&
+              <Dropdown
+                  item
+                  icon ='bars'
+                  key='mobile'
+              >
+                  <Dropdown.Menu>
+                      <Dropdown.Item>
                           <Link href="/">
                               Home
                           </Link>
-                      </Menu.Item>
+                      </Dropdown.Item>
                       { user
-                        && <Menu.Item>
+                        && <Dropdown.Item>
                             <Link href="/overview">
                                 Overview
                             </Link>
-                        </Menu.Item>
+                        </Dropdown.Item>
                       }
                       { user
-                        && <Menu.Item>
+                        && <Dropdown.Item>
                             <Link href="/lectures">
                                 Lectures
                             </Link>
-                        </Menu.Item>
+                        </Dropdown.Item>
                       }
                       { user
-                        && <Menu.Item>
+                        && <Dropdown.Item>
                             <Link href="/stats">
                                 Stats
                             </Link>
-                        </Menu.Item>
+                        </Dropdown.Item>
                       }
                       { user
-                        && <Menu.Item>
+                        && <Dropdown.Item>
                             <Link href="/settings">
                                 Settings
                             </Link>
-                        </Menu.Item>
+                        </Dropdown.Item>
                       }
-                  </>
-
+                  </Dropdown.Menu>
+              </Dropdown>
+            }
+            <Menu.Menu position="right">
+                { user
+                  && <Menu.Item>
+                      <Username name={user.username} />
+                      <ExpPoints points={experience} />
+                  </Menu.Item>
                 }
-                { isMobile &&
-                  <Dropdown
-                      item
-                      icon ='bars'
-                  >
-                      <Dropdown.Menu>
-                          <Dropdown.Item>
-                              <Link href="/">
-                                  Home
-                              </Link>
-                          </Dropdown.Item>
-                          { user
-                            && <Dropdown.Item>
-                                <Link href="/overview">
-                                    Overview
-                                </Link>
-                            </Dropdown.Item>
-                          }
-                          { user
-                            && <Dropdown.Item>
-                                <Link href="/lectures">
-                                    Lectures
-                                </Link>
-                            </Dropdown.Item>
-                          }
-                          { user
-                            && <Dropdown.Item>
-                                <Link href="/stats">
-                                    Stats
-                                </Link>
-                            </Dropdown.Item>
-                          }
-                          { user
-                            && <Dropdown.Item>
-                                <Link href="/settings">
-                                    Settings
-                                </Link>
-                            </Dropdown.Item>
-                          }
-                      </Dropdown.Menu>
-                  </Dropdown>
+                { user
+                  && <Menu.Item>
+                      <Link href="/logout" onClick={handleSignout}>Logout</Link>
+                  </Menu.Item>
                 }
-                <Menu.Menu position="right">
-                    { user
-                      && <Menu.Item>
-                          <Username name={user.username} />
-                          <ExpPoints points={experience} />
-                      </Menu.Item>
-                    }
-                    { user
-                      && <Menu.Item>
-                          <Link href="/logout" onClick={handleSignout}>Logout</Link>
-                      </Menu.Item>
-                    }
-                    { !user
-                      && <Menu.Item>
-                          <Link href="/signup">Sign up</Link>
-                      </Menu.Item>
-                    }
-                    { !user
-                      && <Menu.Item>
-                          <Link href="/login" onClick={handleSignin}>Log in</Link>
-                      </Menu.Item>
-                    }
-                </Menu.Menu>
-            </Menu>
-
-        </div>
-    )
+                { !user
+                  && <Menu.Item>
+                      <Link href="/signup">Sign up</Link>
+                  </Menu.Item>
+                }
+                { !user
+                  && <Menu.Item>
+                      <Link href="/login" onClick={handleSignin}>Log in</Link>
+                  </Menu.Item>
+                }
+            </Menu.Menu>
+        </Menu>
+    );
 }
 
 export default NavMenu;
